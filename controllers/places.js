df9118d6ea8bffa3d7fd392e35c8aa106191f958
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-07-19 10:58:20 -0400

    added places controller

---
+++ controllers/places.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+
+
