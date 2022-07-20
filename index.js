6693a25b20c41771d36432350f64ab80f0875985
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-07-19 21:58:03 -0400

    added render home

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.render('home')
+    res.render('home')
 })
+
 
 app.get('*', (req, res) => {
     res.status(404)('<h1>404 page</h1>')
