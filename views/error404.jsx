e974e87b6f5e79218aa2efa93779e8fb227b8405
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-07-22 13:49:20 -0400

    added images to the pages

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="\images\sadmeme.jpg" height="500px" width="500px" alt="sadmanmeme" />
           </main>
       </Def>
     )
