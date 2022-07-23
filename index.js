74d4af9b8bf26600cc6248607ebd1efbaa48ef18
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-07-23 13:49:03 -0400

    new places can be added

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('view engine', 'jsx')
 app.set('views', __dirname + '/views')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+
 
 
 // Controllers & Routes
