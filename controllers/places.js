786f8b6555df73e7f1c4eba88644f5c4190236f3
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-07-24 09:56:40 -0400

    got new working for the form(i think?)

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,34 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-
-/*GET /places*/
-
+// GET /places
 router.get('/', (req, res) => {
-  res.render('places/index', { places })
+    res.render('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
-
-
 
 
 module.exports = router
 
-
-
-
+// Photo references in
+// {/* Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
+// Photo by <a href="https://unsplash.com/es/@digndoug?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Doug Greenman</a> on <a href="https://unsplash.com/s/photos/coffee-house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+// {/* Photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */ }
